e0298f9f01b7959ebc26077956f387960d857d10
OmarAhmedAdel <omaradel2602@gmail.com>
2024-08-01 15:47:19 +0300

    Working Days's API is working & will adjust it at home!

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createUser,loginUser } = require('../Controllers/userController');
+const {getWorkingDays} = require('../Controllers/workingDays')
 
 
 router.post('/create', createUser);
 router.post('/login',loginUser)
+router.get('/getWorkingDays',getWorkingDays)
+
 
 module.exports = router;
