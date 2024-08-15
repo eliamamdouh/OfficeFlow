3c919a31e46d351dda704699ec0a729acbc5d02c
Omar Ahmed Adel <omaradel2602@gmail.com>
2024-08-15 18:00:17 +0300

    Need to adjust to take the response form token and take the userId from token brdo!

--- Backend/Controllers/viewSchedule.js
+++ Backend/Controllers/viewSchedule.js
@@ -1,19 +1,30 @@
 const { db } = require('../firebase-init');
 const { StatusCodes } = require("http-status-codes");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
 
 const viewSchedule = async (req, res) => {
     try {
-        const { userId } = req.query;
+        const { authorization } = req.headers;
 
-        if (!userId) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'User ID is required' });
+        if (!authorization) {
+            return res.status(StatusCodes.BAD_REQUEST).send('Authorization token is required');
         }
+        console.log('Headers received:', req.headers);
+
+        const token = authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        // Extract the userId from the decoded token
+        const userId = decodedToken.userId; 
+        console.log('Authorization Header:', authorization);
+
 
         const usersCollectionRef = db.collection('Users');
         const userDoc = await usersCollectionRef.doc(userId).get();
 
         if (!userDoc.exists) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
+            return res.status(StatusCodes.NOT_FOUND).send('User not found');
         }
 
         const userData = userDoc.data();
@@ -22,8 +33,9 @@ const viewSchedule = async (req, res) => {
         res.status(StatusCodes.OK).json({ message: 'Schedule retrieved successfully', schedule });
     } catch (error) {
         console.error("Error viewing schedule:", error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Server error', error: error.message });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Server error');
     }
 };
+
 
 module.exports = { viewSchedule };
