a6307a9cd5db199df5426f6dad18e86e59ba7c8a
Omar Ahmed Adel <omaradel2602@gmail.com>
2024-08-22 11:59:43 +0300

    Minor Adjustments!

--- Backend/app.js
+++ Backend/app.js
@@ -3,21 +3,16 @@ const express = require("express");
 const { db } = require("./firebase-init"); // Import the Firebase init file to ensure the connection is established
 
 const app = express();
-const DEFAULT_PORT = 3000; //42192;
+const DEFAULT_PORT = 3000;
 const DEFAULT_HOST = "0.0.0.0";
 const userRoutes = require("./Routes/userRoutes.js");
 const requestRoutes = require("./Routes/requestRoutes.js");
 
 //const {viewRequests}= require('./Controllers/viewRequestsController.js');
 
-const {
-  getTeamMembers,
-  countUsersByLocationOnCurrentDate,
-} = require("./Controllers/userController.js");
+const { getTeamMembers, countUsersByLocationOnCurrentDate } = require("./Controllers/userController.js");
 const { changeSchedule } = require("./Controllers/submitRequestContoller.js");
-const {
-  viewTeamMembersSchedule,
-} = require("./Controllers/Schedule/viewScheduleForTeamMembers.js");
+const { viewTeamMembersSchedule } = require("./Controllers/Schedule/viewScheduleForTeamMembers.js");
 
 const { sendNotification } = require("./firebase-init");
 const { viewNotifications } = require("./Controllers/viewNotifications.js");
