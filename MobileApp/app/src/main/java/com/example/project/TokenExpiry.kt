7296d51aed1883f34c076a20c5e7dfc50a3ed4d0
Omar Ahmed Adel <omaradel2602@gmail.com>
2024-08-22 17:28:53 +0300

    Fix Schedule Error!

--- MobileApp/app/src/main/java/com/example/project/TokenExpiry.kt
+++ MobileApp/app/src/main/java/com/example/project/TokenExpiry.kt
@@ -3,6 +3,7 @@ package com.example.project
 import androidx.navigation.NavController
 
 fun handleTokenExpiration(navController: NavController) {
+
     navController.navigate("splash") {
         popUpTo("page0") { inclusive = true }
     }
